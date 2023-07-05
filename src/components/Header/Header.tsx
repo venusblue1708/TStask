1303c4a263f0f16dc03a4f23de5de4b0584180a9
venusblue1708 <venusblue1708@gmail.com>
2023-07-05 17:40:57 +0600

    feat: added Navbar with styles

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="navbar">
+      <h2 className="navbar_item">Product list Page</h2>
+    </div>
+  );
+};
+
+export { Header };
